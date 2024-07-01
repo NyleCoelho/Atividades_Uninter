0f8f39131b5134e7b9e89975341bebc34544a9e6
biancacoelho <biancacoelhoo05@gmail.com>
2024-07-01 18:43:00 -0400

    Questão 1 implementada

---
+++ Lógica/Exercício1.py
@@ -0,0 +1,28 @@
+#Mensgaem de boas vindas com nome completo
+print('Bem-vindo a loja da Barbara Bianca!')
+
+#Coletando do usuário o valor do pedido e a quantidade de parcelas 
+valorDoPedido = float(input('Entre com o valor do pedido: '))
+quantidadeParcelas = int(input('Entre com a quantidade de parcelas: '))
+
+#Implementando a taxa de juros de acordo com os dados fornecidos
+if quantidadeParcelas >= 4 and quantidadeParcelas < 6:
+    juros = 0.04 #4%
+elif quantidadeParcelas >= 6 and quantidadeParcelas < 9:
+    juros = 0.08 #8%
+elif quantidadeParcelas >= 9 and quantidadeParcelas < 13:
+    juros = 0.16 #16%
+elif quantidadeParcelas >= 13:
+    juros = 0.32 #32%
+else:
+    juros = 0.0
+
+#Implementado o cálculo do valor das parcelas e o valor total parcelado 
+valorDaParcela = valorDoPedido * (1 + juros) / quantidadeParcelas
+valorTotalParcelado = valorDaParcela * quantidadeParcelas
+
+#Apresentando o valor das parcelas e o total parcelado de acordo com a taxa de juros
+print(f'Valor dos juros aplicados e de: {juros * 100:.0f}%')
+print(f'O valor das parcelas é de R$ {valorDaParcela:.2f}')
+print(f'O valor total parcelado é de R$ {valorTotalParcelado:.2f}')
+print('Código por Barbara Bianca Campos Coelho')
